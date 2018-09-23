ea8ce152e47b5ba89a71d20c640073022fd67db3
Victor Samayoa <vhs1092@gmail.com>
2018-09-23 17:13:22 -0600

    add genre, country, year and actors taxonomies to films

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -35,4 +35,71 @@ function create_post_films()
 }
 add_action('init', 'create_post_films');
 
+/**
+ * Register Films taxonomies
+ *  Genre, Country, Year and Actors
+ */
+function register_films_taxonomies()
+{
+    //taxonomies
+    $taxonomies = array(
+        array(
+            'slug' => 'genre_film',
+            'single_name' => 'Genre',
+            'plural_name' => 'Genres',
+            'post_type' => 'films',
+            'rewrite' => array('slug' => 'genre'),
+        ),
+        array(
+            'slug' => 'country_film',
+            'single_name' => 'Country',
+            'plural_name' => 'Countries',
+            'post_type' => 'films',
+            'rewrite' => array('slug' => 'country'),
+        ),
+        array(
+            'slug' => 'year_film',
+            'single_name' => 'Year',
+            'plural_name' => 'Years',
+            'post_type' => 'films',
+            'rewrite' => array('slug' => 'year'),
+        ),
+        array(
+            'slug' => 'actor_film',
+            'single_name' => 'Actor',
+            'plural_name' => 'Actors',
+            'post_type' => 'films',
+            'rewrite' => array('slug' => 'actor'),
+        ),
+    );
 
+    //create taxonomies dinamically
+    foreach ($taxonomies as $taxonomy) {
+        $labels = array(
+            'name' => $taxonomy['plural_name'],
+            'singular_name' => $taxonomy['single_name'],
+            'search_items' => 'Search ' . $taxonomy['plural_name'],
+            'all_items' => 'All ' . $taxonomy['plural_name'],
+            'parent_item' => 'Parent ' . $taxonomy['single_name'],
+            'parent_item_colon' => 'Parent ' . $taxonomy['single_name'] . ':',
+            'edit_item' => 'Edit ' . $taxonomy['single_name'],
+            'update_item' => 'Update ' . $taxonomy['single_name'],
+            'add_new_item' => 'Add New ' . $taxonomy['single_name'],
+            'new_item_name' => 'New ' . $taxonomy['single_name'] . ' Name',
+            'menu_name' => $taxonomy['plural_name'],
+        );
+
+        $rewrite = isset($taxonomy['rewrite']) ? $taxonomy['rewrite'] : array('slug' => $taxonomy['slug']);
+        $hierarchical = isset($taxonomy['hierarchical']) ? $taxonomy['hierarchical'] : true;
+
+        // Register taxonomies
+        register_taxonomy($taxonomy['slug'], $taxonomy['post_type'], array(
+            'hierarchical' => $hierarchical,
+            'labels' => $labels,
+            'show_ui' => true,
+            'query_var' => true,
+            'rewrite' => $rewrite,
+        ));
+    }
+}
+add_action('init', 'register_films_taxonomies');
